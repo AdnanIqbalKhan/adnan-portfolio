7a142aad22aecf7b802f0ce7a6649d3f10369b18
AdnanIqbalKhan <hafizadnaniqbalkhan@gmail.com>
2024-12-23 22:33:45 +0500

    add projects data

--- components/Icons/ExternalLink.tsx
+++ components/Icons/ExternalLink.tsx
@@ -1,8 +1,7 @@
-import {NextRouter} from 'next/router';
-const ExternalLink = (props: { router: NextRouter; url: string }) => {
-    return (
+const ExternalLink = (props: { url: string }) => {
+  return (
+    <a href={props.url} target="_blank" rel="noreferrer">
       <svg
-        onClick={() => props.router.push(props.url)}
         xmlns="http://www.w3.org/2000/svg"
         role="img"
         viewBox="0 0 24 24"
@@ -20,6 +19,7 @@ const ExternalLink = (props: { router: NextRouter; url: string }) => {
         <polyline points="15 3 21 3 21 9"></polyline>
         <line x1="10" y1="14" x2="21" y2="3"></line>
       </svg>
-    );
-  };
+    </a>
+  );
+};
 export default ExternalLink;
